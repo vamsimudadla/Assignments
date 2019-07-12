0a5e409f4d4be34fa3a96d499f8a4fd493441be3
vamsimudadla <mudadlavamsi@gmail.com>
2019-07-12 18:15:33 +0530

    new sub task to prepare todo list

--- todo-list/src/components/TodoList/index.js
+++ todo-list/src/components/TodoList/index.js
@@ -1,25 +1,46 @@
 import React, { Component } from "react";
 import TodoTextBox from "../TodoTextBox";
+import TodoBody from "../TodoBody";
 
 class TodoList extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      todo_items: [],
+      counter: 1
+    };
   }
 
   createTodoObject = todoText => {
     const todo = {
-      id: 1,
+      id: this.state.counter,
       text: todoText,
       completed: false
     };
+    this.setState({
+      todo_items: this.state.todo_items.concat(todo),
+      counter: this.state.counter + 1
+    });
+  };
 
-    console.log(todo);
+  handleCheck = todo_id => {
+    var new_todo_items = this.state.todo_items;
+    for (let i = 0; i < new_todo_items.length; i++) {
+      if (new_todo_items[i].id == todo_id) {
+        new_todo_items[i].completed = true;
+        break;
+      }
+    }
+    this.setState({
+      todo_items: new_todo_items
+    });
   };
 
   render() {
     return (
       <div>
         <TodoTextBox onSaveTodo={this.createTodoObject} />
+        <TodoBody todos={this.state.todo_items} onChecked={this.handleCheck} />
       </div>
     );
   }
